1b868f6783b6e5974a1198953f0307fa0bf2f4a1
Richard Paredes <52340573+richard-paredes@users.noreply.github.com>
2021-05-13 18:10:27 -0500

    fixed prettier problems

--- src/pages/Insights/Insights.js
+++ src/pages/Insights/Insights.js
@@ -7,14 +7,18 @@ const ShowJobCards = (jobs) => {
 	let rows = [];
 	for (let i = 0; i < Math.ceil(jobs.length / 3); i++) {
 		let cards = [];
-		let temp = jobs.slice(i*3, i*3+3);
+		let temp = jobs.slice(i * 3, i * 3 + 3);
 		for (let j = 0; j < temp.length; j++) {
-			cards.push(<Col xs={12} md={4}><JobCard key={j} job={jobs[i*3+j]} /></Col>);
+			cards.push(
+				<Col xs={12} md={4}>
+					<JobCard key={j} job={jobs[i * 3 + j]} />
+				</Col>
+			);
 		}
 		rows.push(<Row key={i}>{cards}</Row>);
 	}
 	return rows;
-}
+};
 
 const Insights = () => {
 	const [searchInput, setSearchInput] = React.useState('');
@@ -28,64 +32,54 @@ const Insights = () => {
 		if (!searchInput) {
 			setJobs(JobsData);
 		} else {
-			setJobs(JobsData.filter(job => job.company.name === searchInput 
-					|| job.title === searchInput 
-					|| job.description === searchInput));
+			setJobs(
+				JobsData.filter(
+					(job) =>
+						job.company.name === searchInput ||
+						job.title === searchInput ||
+						job.description === searchInput
+				)
+			);
 		}
 	};
 
 	/** Need some state to fetch and populate a list of jobs */
 	return (
-		<Container fluid className="main">
-			<Jumbotron fluid className="hero">
+		<Container fluid className='main'>
+			<Jumbotron fluid className='hero'>
 				<h1>Cougar Insights</h1>
 			</Jumbotron>
-			<Container className="secondary container">
-				<Container className="search-content">
-					<div className="search">
-						<div className="search-input">
+			<Container className='secondary container'>
+				<Container className='search-content'>
+					<div className='search'>
+						<div className='search-input'>
 							<input value={searchInput} onChange={updateSearchInput} />
-							<button onClick={handleSearch}>
-								Search
-							</button>
+							<button onClick={handleSearch}>Search</button>
 						</div>
-						<h6>
-							{jobs.length} jobs found
-						</h6>
+						<h6>{jobs.length} jobs found</h6>
 					</div>
 				</Container>
-				<Container className="job-content">
-					{
-						ShowJobCards(jobs)
-					}
-				</Container>
+				<Container className='job-content'>{ShowJobCards(jobs)}</Container>
 			</Container>
 		</Container>
 	);
-}
+};
 export default Insights;
 
 const JobCard = ({ job }) => {
 	const { company, title, description } = job;
 
-	return <Card className="job-card">
-		<Card.Body>
-			<Card.Title>
-				<div>
-
-				</div>
-				<div>
-					{company.name}
-				</div>
-			</Card.Title>
-			<Card.Subtitle className="mb-2 text-muted">
-				{title}
-			</Card.Subtitle>
-			<Card.Text>
-				{description}
-			</Card.Text>
-			<Card.Link href="#"></Card.Link>
-		</Card.Body>
-
-	</Card>
-}
+	return (
+		<Card className='job-card'>
+			<Card.Body>
+				<Card.Title>
+					<div></div>
+					<div>{company.name}</div>
+				</Card.Title>
+				<Card.Subtitle className='mb-2 text-muted'>{title}</Card.Subtitle>
+				<Card.Text>{description}</Card.Text>
+				<Card.Link href='#'></Card.Link>
+			</Card.Body>
+		</Card>
+	);
+};
--- src/pages/Insights/jobs.js
+++ src/pages/Insights/jobs.js
@@ -14,105 +14,106 @@
 // ];
 
 export const jobs = [
-    {
-        company: {
-            name: 'Apple'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'https://uhouston-csm.symplicity.com/students/app/jobs/detail/8c180151b9f28632b75ddda121659a77'	//button	
-    },
-    {
-        company: {
-            name: 'Microsoft'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button				
-    },
-    {
-        company: {
-            name: 'Google'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button				
-    },
-    {
-        company: {
-            name: 'Netflix'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
-    {
-        company: {
-            name: 'Chevron'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button				
-    },
-    {
-        company: {
-            name: 'ExxonMobile'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
-    {
-        company: {
-            name: 'Shell'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
-    {
-        company: {
-            name: 'Fidelity'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
-    {
-        company: {
-            name: 'JP Morgan & Chase'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
-    {
-        company: {
-            name: 'Bank of America'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button		
-    },
-    {
-        company: {
-            name: 'Bank of America'
-        },
-        title: 'Software Engineer',
-        location: "",
-        description: 'A very concise job description...',//job board link???
-        ApplyNow: 'hyperLink'	//button			
-    },
+	{
+		company: {
+			name: 'Apple',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow:
+			'https://uhouston-csm.symplicity.com/students/app/jobs/detail/8c180151b9f28632b75ddda121659a77', //button
+	},
+	{
+		company: {
+			name: 'Microsoft',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Google',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Netflix',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Chevron',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'ExxonMobile',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Shell',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Fidelity',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'JP Morgan & Chase',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Bank of America',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
+	{
+		company: {
+			name: 'Bank of America',
+		},
+		title: 'Software Engineer',
+		location: '',
+		description: 'A very concise job description...', //job board link???
+		ApplyNow: 'hyperLink', //button
+	},
 ];
 
 // How to geta direct access link to cougar pathways from job link?
@@ -131,5 +132,3 @@ export const jobs = [
 //    description: 'A very concise job description...',//job board link???
 //    ApplyNow: 'hyperLink'	//button
 // ];
-
-
